7b5cc7d656496cae629e18393d6fb91c9d2d370e
Jan Zhanal <34436593+janzhanal@users.noreply.github.com>
2020-05-28 23:32:59 +0200

    Init

---
+++ crop.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import sys, getopt
+from PIL import Image
+
+def main():
+    tiff = sys.argv[1]
+    templates = sys.argv[2]
+    opened_tiff = Image.open(tiff)
+    width, height = opened_tiff.size
+    templ = (get_template(templates))
+    crop(tiff, width, height, templ, transfer(width,height,templ)[0], transfer(width,height,templ)[1] )
+
+"""
+Get axes settings from loaded template
+"""
+def get_template (template_paths):
+    axes = []
+    i=0
+    with open(template_paths) as file:
+        for line in file:
+            axes.insert(i,line.split())
+            i += 1
+    return axes
+
+"""
+Crop one box from whole image
+"""
+def crop (tiff_path, imwidth, imheight, templ, x_unit, y_unit):
+    print (imwidth, imheight)
+    top = 0
+    left = 0
+    opened_image = Image.open(tiff_path)
+    for stepx in templ[0]:
+        right = stepx
+        top = 0
+        for stepy in templ[1]:
+            bottom = stepy
+            testovaci = opened_image.crop((int(left)*x_unit, int(top)*y_unit, int(right)*x_unit-1, int(bottom)*y_unit))
+            obr = (tiff_path, right, "x", bottom, ".jpg")
+            name = ("".join(obr))
+            #print ("left=",int(left)*x_unit,"top=", int(top)*y_unit,"right=", int(right)*x_unit-1,"bottom=", int(bottom)*y_unit)
+            testovaci = testovaci.save(str(name))
+            
+            top = stepy
+        left = stepx
+        
+        
+    return "zatim nic"
+
+"""
+Get main unit (image size / template size)
+"""
+def transfer (imwidth,imheight,templ):
+    x_size = len(templ[0])
+    y_size = len(templ[1])
+    x_unit = imwidth // int(templ[0][x_size-1])
+    y_unit = imheight // int(templ[1][y_size-1])
+    return (x_unit, y_unit)
+
+if __name__ == "__main__":
+   main()
